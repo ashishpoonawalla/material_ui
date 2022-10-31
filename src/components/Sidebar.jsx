b9c95422d2746d576af63d4bcc260c4b0c6a44d4
Ashish <ashish.omar.pune@gmail.com>
2022-10-31 19:54:41 +0530

    Init

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <Box bgcolor="skyblue" flex={2}>
+      Sidebar
+    </Box>
+  )
+}
+
+export default Sidebar
